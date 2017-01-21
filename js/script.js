f809bfb5216feea73a12126ffe08b3164a4fd3ba
lgvallejos <gabrielvallejos86@gmail.com>
2017-01-21 19:32:23 -0300

    Reindentado js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 
 $( document ).ready(function() {
-var i=0;
+    var i=0;
 
     $('#alta').click(function() {
-var fields = {};
-$("#formulario").find(":input").each(function() {
+        var fields = {};
+        $("#formulario").find(":input").each(function() {
     // The selector will match buttons; if you want to filter
     // them out, check `this.tagName` and `this.type`; see
     // below
@@ -17,70 +17,70 @@ $("#formulario").find(":input").each(function() {
     
 });
 
-i++;
+        i++;
 
-fields[this.id]=i;
-var filename = fields.foto.split('\\').pop();
+        fields[this.id]=i;
+        var filename = fields.foto.split('\\').pop();
 //alert(filename);
 var obj = {fields: fields};
 
 $('.col-md-8').append('<div class="media"><div class="media-left"><a href="#"><img class="media-object" src="assets/'+filename+'" alt="foto usuario"></a></div><div class="media-body"><p><b>Nombre:</b> '+fields.nombre+'</p><p><b>Apellido:</b> '+fields.apellido+'</p><p><b>Edad:</b> '+fields.edad+'</p><p></p></div><div class="foot-media"><b>Dirección:</b> '+fields.direccion+'</div><span class="elimino glyphicon glyphicon-remove" style="cursor:pointer;display:none;"></span></div>');
 localStorage.setItem('registro'+i,JSON.stringify(fields)); 
- $(".media").animate({
-        width: "45%",
-		height: "295px"
-    }, 500, function() {
+$(".media").animate({
+    width: "45%",
+    height: "295px"
+}, 500, function() {
     $('.elimino').show();
     // Animation complete.
     console.log('animacion completada');
-  });
+});
 $('#formulario')[0].reset();
 //alert(JSON.stringify(fields));
 
 });
 
 
-$(document).on('click','.elimino',function(){
-    $(this).hide();
-	$(this).parent().animate({
-        width: "0",
-		height: "0"
+    $(document).on('click','.elimino',function(){
+        $(this).hide();
+        $(this).parent().animate({
+            width: "0",
+            height: "0"
+        }, 500, function() {
+            $(this).remove();
+
+    // Animation complete.
+    console.log('animacion completada');
+});
+        
+        
+    });
+
+    $(document).on('click','.leftarrow',function(){
+       $('.col-md-4').animate({
+        opacity: 0.25,
+        left: "-=50",
+        height: "toggle"
     }, 500, function() {
-    $(this).remove();
-
+        $('.rightarrow').show();
     // Animation complete.
     console.log('animacion completada');
-  });
-     
-	
 });
+       
+   });
 
-$(document).on('click','.leftarrow',function(){
-     $('.col-md-4').animate({
-    opacity: 0.25,
-    left: "-=50",
-    height: "toggle"
-  }, 500, function() {
-    $('.rightarrow').show();
+    $(document).on('click','.rightarrow',function(){
+        $('.rightarrow').hide();
+        $('.col-md-4').animate({
+            opacity: 1,
+            left: "+=50",
+            height: "toggle"
+        }, 500, function() {
+            
     // Animation complete.
     console.log('animacion completada');
-  });
-	
 });
-
-$(document).on('click','.rightarrow',function(){
-    $('.rightarrow').hide();
-     $('.col-md-4').animate({
-    opacity: 1,
-    left: "+=50",
-    height: "toggle"
-  }, 500, function() {
-    
-    // Animation complete.
-    console.log('animacion completada');
-  });
-    
-});
+        
+    });
 
 
 });
